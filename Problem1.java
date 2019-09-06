a4c474eff90e1cc7297636f2188162cd7ce985f9
Jimmy Phan <jimmyphan777@gmail.com>
2019-09-06 17:46:24 -0500

    first commit

---
+++ Problem1.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+package assignment1;
+
+class Problem1{
+    public static void main(String args[]){
+        /*  Input: A 1000-digit number to search through
+         *  Output: prints largest product of the at most n adjacent digits
+         */
+        int largestProd = 0;
+        String largestProd_str = "";
+        String num_str;
+
+        // retrieve number
+        if(args[0].isEmpty()){
+            System.out.println("Forgot the number");
+        }
+        else{
+            num_str = args[0];
+        }
+
+        ArrayList<String> zero_separated_strings = new ArrayList<String>();
+
+    }
+
+    public int substring_product(String s){
+        /*  Input: substring of a series of numbers
+         *  Output: product of all the digits
+         */
+        return 0;
+    }
+}
